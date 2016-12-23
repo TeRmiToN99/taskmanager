4d37d4763c0a80d2585752804ca27bdf4090d84a
TeRmiT <termit99@bk.ru>
2016-12-23 14:37:00 +0300

    Добавление шаблона в index.php

--- index.php
+++ index.php
@@ -1,16 +1,2 @@
 <?php
-use \App\Models\User;
-require __DIR__ . '/autoload.php';
-
-/*$users = \App\Models\User::findAll();
-
-function sendMail(User $user, string $message){
-    echo 'Почта с текстом: ' . $message . '<br>' . 'на почту: '  . $user->email;
-}
-
-sendMail($users[0], 'Какойто не очень приятный текст..');
-*/
-$user = new User();
-$user->name = 'Feday';
-$user->email = 'feday@exemple.com';
-$user->insert();
+require __DIR__ . '/App/templates/index.php';
